ce524e88d1ed1f5f553cb7101e5838d44bedb0fa
Zlatka Dimitrova <113666852+zlatix17@users.noreply.github.com>
2023-02-23 08:35:20 +0200

    neshto za decimala dobavih

--- Data/Product.cs
+++ Data/Product.cs
@@ -1,5 +1,12 @@
-﻿namespace DZIproject.Data
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DZIproject.Data
 {
+    public enum Gender
+        {
+            Women, Men
+        }
     public class Product
     {
         public int Id { get; set; }
@@ -9,10 +16,8 @@
         public string Size { get; set; }
         public int Quantity { get; set; }
         public string Description { get; set; }
-        public enum Gender
-        {
-            Women, Men
-        }
+
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal Price { get; set; }
         public DateTime RegisterOn { get; set; }
         ICollection<Shopping> Shoppings { get; set; }
